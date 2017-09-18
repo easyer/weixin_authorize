564f187a1eeebe2c5d1e6254d0203c2a7c9b5d3d
xiexh <xiexh@wikiflyer.cn>
2017-09-18 22:53:57 +0800

    测试token

--- lib/weixin_authorize/token/redis_store.rb
+++ lib/weixin_authorize/token/redis_store.rb
@@ -21,6 +21,8 @@ module WeixinAuthorize
           client.expired_at
         )
         weixin_redis.expireat(client.redis_key, client.expired_at.to_i)
+
+        Rails.logger.info("weixin_redis==@{weixin_redis}")
       end
 
       def access_token
@@ -29,6 +31,10 @@ module WeixinAuthorize
 
         client.access_token = weixin_redis.hget(client.redis_key, "access_token")
         client.expired_at   = weixin_redis.hget(client.redis_key, "expired_at")
+
+        Rails.logger.info("client.access_token==@{client.access_token}")
+        Rails.logger.info("client.expired_at==@{client.expired_at}")
+
         client.access_token
       end
 
